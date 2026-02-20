32fcefba6e4e622ec45d1776ed11e882180c0997
CDiegoSV <cdsalgadov@gmail.com>
2026-02-20 12:27:16 -0600

    feat (PlayerController): Movimiento basico del jugador.

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,6 +1,41 @@
-using UnityEngine;
+using UnityEngine;}
+
+/// <summary>
+/// Players basic movement.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    #region Knobs
+
     public float speed = 5.0f;
-    void Update() { }
+
+    #endregion
+
+    #region Runtime Variables
+
+    private Rigidbody2D m_playerRb;
+
+    private Vector2 m_moveDirection;
+
+    #endregion
+
+    void Update() 
+    {
+        Move();
+    }
+
+    void FixedUpdate()
+    {
+        m_playerRb.MovePosition(m_playerRb.position + speed * Time.fixedDeltaTime * m_moveDirection)
+    }
+
+    private void Move()
+    {
+        if (m_moveDirection.magnitude  > 0) {
+            float m_movementX = Input.GetAxisRaw("Horizontal");
+            float m_movementY = Input.GetAxisRaw("Vertical");
+
+            m_moveDirection = new Vector2(m_movementX, m_movementY).normalized;
+        }
+    }
 }
