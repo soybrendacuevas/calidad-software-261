e472ac9a7fc0d9d81bf035047b83c209c35757c6
Chicken021 <cmhernandez@cocoschool.mx>
2026-02-25 16:43:41 -0600

    Implementacion del movimiento del Jugador

---
+++ PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Variables
+    public float speed = 5.0f;
+    [SerializeField] RigidBody rb;
+    #endregion
+
+
+    #region Unity Methods
+
+    void Start ()
+    {
+        Intialize();
+    }
+
+
+
+    void Update(){
+
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer  Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor * speed , rb.velocity.y,moveVer * speed);
+
+        rb.velocity = movement;
+    }
+    #endregion
+
+    #region Functions
+    void Intialize()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    #endregion
+}
