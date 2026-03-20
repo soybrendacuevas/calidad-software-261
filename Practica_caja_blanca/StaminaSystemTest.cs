40b9c1e64bbf95d3f67c5049665a13ad2920930e
Koda <kodaxd09@gmail.com>
2026-03-20 13:12:55 -0600

    Preparacion de codio staminasystemtest

--- Practica_caja_blanca/StaminaSystemTest.cs
+++ Practica_caja_blanca/StaminaSystemTest.cs
@@ -1,6 +1,14 @@
 ﻿using System;
-
+using NUnit.FrameWork; 
+[TestFixture]
 public class StaminaSystemTest
 {
-	
+	[Test]
+	public void UseStamina_WhenAmountExceedsCurrent_ShouldClampToZero() {
+        //ARRANGE 
+
+        //ACT 
+
+        //ASSERT 
+    }
 }
