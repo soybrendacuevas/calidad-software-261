58db6325d57c17ae4a33767eeb03e31cf089fbd0
koda <kodaxd09@gmail.com>
2026-03-27 13:00:38 -0600

    Implementación de patrones y estructura de seguridad para el codigo de PlayerController

--- Main/Patrones de desarrollo/practica-técnica/PlayerController.cs
+++ Main/Patrones de desarrollo/practica-técnica/PlayerController.cs
@@ -1,17 +1,62 @@
 using UnityEngine;
-
+public enum FSMHealthStatus {
+    GoodHealth,
+    LowHealth,
+    DeadState,
+}
 public class PlayerController : MonoBehaviour {
     // Las variables de health y isDead son públicas, por lo que permiten manipulación 
-    public float health = 100; 
-    public bool isDead = false;
+    [SerializedField] private float health = 100; 
+    [SerializedField] private float maxHealth = 100;
+    [SerializedField] private bool isDead = false;
+    [SerializedField] private FSMHealthStatus healthStatus; 
     // El input está dentro del Update, por lo que cada frame verificará si se está presionando 
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.H)) {
-            health += 50; 
+    private void Start () {
+        switch(health) {
+            case health > 50:
+                healthStatus = FSMHealthStatus.GoodHealth;
+                break;
+            case health < 51 && health > 0:
+                healthStatus = FSMHealthStatus.LowHealth;
+                break;
+            case health < 0:
+                healthStatus= FSMHealthStatus.DeadState;
+                break; 
         }
     }
+    private void Update() {
+        switch (health) {
+            case health > 50:
+                healthStatus = FSMHealthStatus.GoodHealth;
+                break;
+            case health < 51 && health > 0:
+                healthStatus = FSMHealthStatus.LowHealth;
+                break;
+            case health < 0:
+                healthStatus = FSMHealthStatus.DeadState;
+                break;
+        }
+        switch (healthStatus) {
+            case FSMHealthStatus.GoodHealth:
+            case FSMHealthStatus.LowHealth:
+                if (Input.GetKeyDown(KeyCode.H)) {
+                    health += 50; 
+                }
+                break;
+            case FSMHealthStatus.DeadState:
+                break; 
+        }
+        
+    }
     // SaveGame() está de forma pública
-    public void SaveGame() {
+    private void SaveGame() {
            PlayerPrefs.SetFloat("PlayerHealth", health); 
+    }
+    private void ApplyHealing(float amount) {
+        if(amount < 0) {
+            Debug.log("No se aceptan Valores negativos");
+            return; 
+        }
+        health += Mathf.Clamp(amount, 0, maxHealth);
     }
 }
