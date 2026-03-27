ce46324683e5417b71d409cbc85d27c4cf794439
Koda <kodaxd09@gmail.com>
2026-03-27 07:50:12 -0600

    Finalización de código de caja blanca

--- Practica_caja_blanca/StaminaSystemTest.cs
+++ Practica_caja_blanca/StaminaSystemTest.cs
@@ -6,9 +6,20 @@ public class StaminaSystemTest
 	[Test]
 	public void UseStamina_WhenAmountExceedsCurrent_ShouldClampToZero() {
         //ARRANGE 
-
+        var sistema = new StaminaSystem();
         //ACT 
-
+        sistema.UseStamina(1000f);
         //ASSERT 
+        Assert.AreEqual(0, sistema.CurrentStamina, "El valor es menos al esperado").
+    }
+
+    [Test]
+    public void Regenerate_WhenAmountExceedsCurrent_ShouldClampToMaxStamina() {
+        //ARRANGE 
+        var sistema = new StaminaSystem();
+        //ACT 
+        sistema.RegenerateStamina(1000f);
+        //ASSERT 
+        Assert.AreEqual(100, sistema.CurrentStamina, "El valor es mayor al esperado").
     }
 }
