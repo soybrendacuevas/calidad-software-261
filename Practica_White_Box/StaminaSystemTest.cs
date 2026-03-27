7a174a34ebc3aa31d24356e5cfceae53d792ec54
mauciello <mauciello@cocoschool.mx>
2026-03-27 08:23:13 -0600

    Codigo de caja blanca

---
+++ Practica_White_Box/StaminaSystemTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+[TestFixture]
+public class StaminaSystemTest
+{
+	[Test]
+	public void UseStamina_WhenAmountExceedsCurrent_ShouldClampToZero() {
+		var sistema = new StaminaSystem();
+		sistema.UseStamina(100f);
+		Assert.AreEqual(0, sistema.CurrentStamina, "El valor es menos del esperado");
+	}
+
+	[Test]
+	public void Regenerate_WhenAmountExceedsCurrent_ShouldClampToMaxStamina() {
+		var sistema = new StaminaSystem();
+        sistema.RegenerateStamina(100f);
+        Assert.AreEqual(0, sistema.CurrentStamina, "El valor es mayor del esperado");
+    }
+}
